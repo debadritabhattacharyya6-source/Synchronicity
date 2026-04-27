36b6066632479b47267fd162a2bb6defce293950
TanavB <tanavbhatter.pkt@gmail.com>
2026-04-27 23:28:30 +0530

    Added Google SignIn button

---
+++ src/assets/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRKKhIO8m-3ikgfd8Xjj9J13P8GVl3VgA",
+  authDomain: "sync-space-fb0fb.firebaseapp.com",
+  projectId: "sync-space-fb0fb",
+  storageBucket: "sync-space-fb0fb.firebasestorage.app",
+  messagingSenderId: "26340060828",
+  appId: "1:26340060828:web:4602b9658c35c11315c759",
+  measurementId: "G-LBRWJ4QMDF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth();
+export const googleProvider = new GoogleAuthProvider();
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
 import "./Auth.css";
 import logo from "/src/assets/syncspace-logo.png"; // adjust path
+import { auth, googleProvider } from "/src/assets/firebase"
+import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { FcGoogle } from "react-icons/fc";
 
 export default function Auth({ mode, setMode, onComplete }) {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const signUp = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const signUpWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+    onComplete();
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Here we bypass real auth and go to dashboard
+    if (mode === "signup") {
+      signUp();
+    }
+    else if (mode === "login") {
+
+    }
     onComplete();
   };
 
@@ -21,27 +43,27 @@ export default function Auth({ mode, setMode, onComplete }) {
       <div className="auth-container">
         <img src={logo} alt="syncspace" className="auth-logo" />
         <h2 className="auth-title">{mode === "login" ? "Welcome Back" : "Create Account"}</h2>
-        
+
         <form onSubmit={handleSubmit} className="auth-form">
           <div className="input-group">
-            <label htmlFor="username">Username</label>
-            <input 
-              type="text" 
-              id="username" 
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required 
-              placeholder="Enter your username"
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Enter your email"
             />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input 
-              type="password" 
-              id="password" 
+            <input
+              type="password"
+              id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              required 
+              required
               placeholder="Enter your password"
             />
           </div>
@@ -49,6 +71,8 @@ export default function Auth({ mode, setMode, onComplete }) {
             {mode === "login" ? "LOG IN" : "SIGN UP"}
           </button>
         </form>
+        <p className="auth-or">Or</p>
+        <button className="auth-google" onClick={signUpWithGoogle}><FcGoogle className="logo-img-google" />Sign in With Google</button>
 
         <p className="auth-toggle">
           {mode === "login" ? "Don't have an account? " : "Already have an account? "}
